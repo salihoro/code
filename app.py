dce1d21eb5a34fef031bca353eecffb959edeb6b
Salih Horozovic <Sali@Salihs-MBP.attlocal.net>
2016-10-22 15:34:59 -0500

    added files

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+
+
+@app.route('/')
+# @app.route('/<name>')
+def index():
+    return render_template("index.html")
+
+
+# @app.route('/<myth>')
+# # @app.route('/<name>')
+# def myth(myth='Myth'):
+#     return render_template("myth.html", myth = myth)
+
+
+@app.route('/myth')
+# @app.route('/<name>')
+def myth():
+    return render_template("myth.html")
+
+@app.route('/hercules')
+# @app.route('/<name>')
+def hercules():
+    return render_template("hercules.html")
+
+
+app.run(debug=True, port=8000, host='0.0.0.0')
